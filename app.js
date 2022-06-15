e9fb22c115cee50145e8f8d4a5a4af5a03adc1fb
soneoso <eli.dekeersgieter@gmail.com>
2022-06-15 16:36:43 +0200

    exercises

---
+++ app.js
@@ -0,0 +1,107 @@
+// console.log ('hello');
+
+// const b = 'smoothie';
+// console.log (b);
+
+// let someNumber = 45;
+// console.log (someNumber);
+
+// Manipulate DOM with JS
+// It's just a fancy way of saying change HTML with javascript
+
+// const age = prompt('What is your age?');
+
+// document.getElementById('someText').innerHTML = age;
+
+
+// Numbers in JS
+
+// let num1 = 10;
+
+// increment num1 by 1
+
+// num1++;
+
+// decrement num1 by 1
+
+// num1--;
+
+// console.log (num1);
+
+// Divide, multiply *, remainder %
+
+// console.log (num1 / 6);
+
+// increment/decrement by any numer you want
+
+// num1;
+// console.log (num1);
+
+
+/*
+1. create a function
+2. call the function
+*/
+
+// create a function
+
+// function fun() {
+//     console.log ('this is a function');
+// }
+
+// call
+// fun ();
+
+/* let's create a function that take in a name
+and says hello followed by your name
+
+for example 
+
+Name: "Eli"
+Return: "Hello Eli"
+*/
+
+
+// function greeting (yourName) {
+//     const result = 'Hello' + ' ' + yourName; // string concatenation
+//     console.log (result);
+// }
+// const name = prompt ('What is your name?');
+// greeting (name);
+
+
+// How do arguments work in functions?
+
+// How do we add 2 numbers together in a function?
+
+// function sumNumbers (num1, num2) {
+//     const result = num1 + num2;
+//     console.log (result);
+// }
+
+// sumNumbers (10, 10);
+
+// while loops
+
+// let num = 0;
+
+// while (num < 100) {
+//     num += 1;
+//     console.log (num);
+// }
+
+
+// for loop
+
+// for (let num = 0; num <= 100; num++) {
+//     console.log (num);
+// }
+
+
+// data types
+
+let yourAge = 18; // numbers
+let yourName = 'bob'; // string
+let name = {first: 'jane', last: 'doe'}; // object
+
+
