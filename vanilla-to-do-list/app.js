165b0008f3f5df0226ad7d8035263af26ffec08b
soneoso <eli.dekeersgieter@gmail.com>
2022-06-17 15:22:08 +0200

    addTodo function and more styling

---
+++ vanilla-to-do-list/app.js
@@ -0,0 +1,38 @@
+//Selectors
+
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+
+function addTodo(event) {
+    //Prevent form from submitting
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo")
+    //create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    // CHECK mark BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+        // CHECK trash BUTTON
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+        // APPEND to LIST
+        todoList.appendChild(todoDiv);
+        //Clear Todo INPUT VALUE
+        todoInput.value = "";
+}
