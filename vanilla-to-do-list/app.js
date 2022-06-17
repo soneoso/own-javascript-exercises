47f438ef97ed818bbb7d34e6a4e6641d71b74867
soneoso <eli.dekeersgieter@gmail.com>
2022-06-17 16:38:43 +0200

    trash and complete button functionality

--- vanilla-to-do-list/app.js
+++ vanilla-to-do-list/app.js
@@ -7,6 +7,7 @@ const todoList = document.querySelector(".todo-list");
 //Event Listeners
 
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //Functions
 
@@ -35,4 +36,25 @@ function addTodo(event) {
         todoList.appendChild(todoDiv);
         //Clear Todo INPUT VALUE
         todoInput.value = "";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    //DELETE TODO
+    if(item.classList[0] === "trash-btn") {
+       const todo = item.parentElement;
+       // ANIMATION
+       todo.classList.add("fall");
+       todo.addEventListener('transitionend', function() {
+           todo.remove()
+       });
+       
+    }
+
+    //CHECK MARK
+
+    if(item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
